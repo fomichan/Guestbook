89f48dd64d4e74f843932d1b7f1e25dfcbfc2101
Aleksei <fomich.alexey@yandex.ru>
2023-04-06 22:08:21 +0200

    Add the index controller

--- src/Kernel.php
+++ src/Kernel.php
@@ -4,8 +4,13 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use function Symfony\Component\DependencyInjection\Loader\Configurator\iterator;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
 }
+
+
+
